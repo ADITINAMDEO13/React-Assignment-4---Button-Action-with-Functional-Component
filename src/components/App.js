dd823ada9ead2fad48bf192715fc182016c3310e
ADITINAMDEO13 <68394896+ADITINAMDEO13@users.noreply.github.com>
2021-02-24 15:16:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,22 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [isClicked,setClick]=useState(false);
-  const handleClick=()=>{
-    setClick({isClicked:true})
+  const [paraState , setParaState] = useState(false);
+
+   const handleClick = () =>{
+    setParaState(true);
   }
-  const addPara=()=>{
-    if(isClicked){
-      return <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-    }
-  }
+
   return (
     <div id="main">
-      {/* // Do not alter the main div */}
-      <button id="click" onClick={handleClick}>click me</button>
-      {/* {addPara()} */}
-      {isClicked && <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+        <button id="click" onClick={handleClick}>Click It Once</button>
+        {
+          paraState ? 
+
+          <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+          :
+          null
+        }
     </div>
   );
 }
